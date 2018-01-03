659e888fb4c3f573e72380b502ea93535916d852
Kaarel Moppel <kaarel.moppel@gmail.com>
2018-01-03 16:27:20 +0200

    Docker image without "root"

--- pgwatch2/bootstrap/launch_wrapper.sh
+++ pgwatch2/bootstrap/launch_wrapper.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-# currently only checks if SSL is enabled and if so generates new cert on the first run
-if [ -n "$PW2_WEBSSL" -o -n "$PW2_GRAFANASSL" ] ; then
-    if [ ! -f /pgwatch2/ssl_key.pem -o ! -f /pgwatch2/ssl_cert.pem ] ; then
-        openssl req -x509 -newkey rsa:4096 -keyout /pgwatch2/ssl_key.pem -out /pgwatch2/ssl_cert.pem -days 3650 -nodes -sha256 -subj '/CN=pw2'
-    fi
+# generates new cert on the first run
+# used by Postgres and also WebUI/Grafana if $PW2_WEBSSL/$PW2_GRAFANASSL set
+
+if [ ! -f /pgwatch2/ssl_key.pem -o ! -f /pgwatch2/ssl_cert.pem ] ; then
+    openssl req -x509 -newkey rsa:4096 -keyout /pgwatch2/ssl_key.pem -out /pgwatch2/ssl_cert.pem -days 3650 -nodes -sha256 -subj '/CN=pw2'
+    chmod 0600 /pgwatch2/*.pem
 fi
 
-if [ -n "$PW2_GRAFANASSL" ] ; then
-    sed -i 's/protocol = http.*/protocol = https/' /etc/grafana/grafana.ini
+if [ -n "$PW2_GRAFANASSL"] ; then
+    HTTP=$(grep -c 'protocol = http' /etc/grafana/grafana.ini)
+    if [ "$HTTP" -eq 0 ] ; then
+        sed -i 's/protocol = http.*/protocol = https/' /etc/grafana/grafana.ini
+    fi
 fi
 
 supervisorctl start grafana
